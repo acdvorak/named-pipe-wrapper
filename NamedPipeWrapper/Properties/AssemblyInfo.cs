a44fd3b201cef13191a89565d5d9a6cef8e642e5
Andrew C. Dvorak <acdvorak@gmail.com>
2013-09-29 14:37:17 -0500

    Updated assembly description

--- NamedPipeWrapper/Properties/AssemblyInfo.cs
+++ NamedPipeWrapper/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NamedPipeWrapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Named Pipe Wrapper library")]
+[assembly: AssemblyDescription("A simple, easy to use, strongly-typed wrapper around .NET named pipes.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NamedPipeWrapper")]
