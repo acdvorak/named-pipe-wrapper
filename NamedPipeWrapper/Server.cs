05c55bb5402fd0f871fd5ae544c12ccf66ca7a2a
Andrew C. Dvorak <acdvorak@gmail.com>
2013-09-29 19:31:23 -0500

    Minor/ignore - Removed unused 'using's

--- NamedPipeWrapper/Server.cs
+++ NamedPipeWrapper/Server.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using NamedPipeWrapper.IO;
 using NamedPipeWrapper.Threading;
 
