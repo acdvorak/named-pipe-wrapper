e687a04040d3855d8796be571d004e661bb728b5
JJW <jw.jeong@swinnus.com>
2021-09-07 09:56:28 +0900

    The project type was changed to .Net Standard 2.1 and .Net Core 3.1.

--- ExampleCLI/MyClient.cs
+++ ExampleCLI/MyClient.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NamedPipeWrapper;
+﻿using NamedPipeWrapper;
+using System;
 
 namespace ExampleCLI
 {
--- NamedPipeWrapper/NamedPipeServer.cs
+++ NamedPipeWrapper/NamedPipeServer.cs
@@ -280,7 +280,7 @@ namespace NamedPipeWrapper
 
         public static NamedPipeServerStream CreatePipe(string pipeName, PipeSecurity pipeSecurity)
         {
-            return new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous | PipeOptions.WriteThrough, 0, 0, pipeSecurity);
+            return new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous | PipeOptions.WriteThrough, 0, 0);
         }
     }
 }
